d174be1d052422779c3244bafd0a8d4df1e81d43
Виктор Тузовский <vtuzovskiy@fura.ru>
2023-09-28 18:33:54 +0700

    Added review views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,47 @@
-from django.db.models import F, OuterRef, Prefetch
-from django_filters import rest_framework as dj_filters
-from rest_framework import mixins
-from rest_framework.viewsets import GenericViewSet
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
+from api.serializers import ReviewSerializer, ReviewListSerializer
+from products.models import Product, Review
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return ReviewListSerializer
+        return ReviewSerializer
+
+    @action(
+        detail=True, methods=['POST'], permission_classes=[IsAuthenticated]
+    )
+    def favorite(self, request, pk):
+        user = request.user
+        product = get_object_or_404(Product, id=pk)
+        model_obj = get_object_or_404(Review, user=user, product=product)
+        model_obj.is_favorite = True
+        model_obj.save()
+        return Response(status=status.HTTP_200_OK)
+
+    @favorite.mapping.delete
+    def delete_favorite(self, request, pk):
+        user = request.user
+        product = get_object_or_404(Product, id=pk)
+        model_obj = get_object_or_404(Review, user=user, product=product)
+        model_obj.is_favorite = False
+        model_obj.save()
+        return Response(status=status.HTTP_200_OK)
+
+    def get_queryset(self):
+        product_id = self.kwargs.get('product_id')
+        product = get_object_or_404(Product, pk=product_id)
+        return product.reviews.all()
+
+    def perform_create(self, serializer):
+        product_id = self.kwargs.get('product_id')
+        product = get_object_or_404(Product, pk=product_id)
+        serializer.save(user=self.request.user, product=product)
