ab0fc3631f6a322a9ae3743763667fd282fe6259
Виктор Тузовский <vtuzovskiy@fura.ru>
2023-09-29 22:39:11 +0700

    Added review urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import CategoryAPIView, ProductAPIView, OrderViewSet
+from api.views import CategoryAPIView, ProductAPIView, OrderViewSet, \
+    ReviewViewSet
 
 router = routers.DefaultRouter()
 router.register('categories', CategoryAPIView, basename='categories')
 router.register('products', ProductAPIView, basename='products')
 router.register('orders', OrderViewSet, basename='orders')
+router.register('reviews', ReviewViewSet, basename='reviews')
 
 urlpatterns = [
     path('', include(router.urls)),
