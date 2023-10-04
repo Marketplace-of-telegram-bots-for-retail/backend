6283c9a50d80dc31eda7cc1ffbe7bd69a3a4a8e5
Артем Пилипенко <artem38skull@yandex.ru>
2023-10-04 20:43:41 +0800

    Preparing for the review

--- backend/core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-
-class BaseAdmin(admin.ModelAdmin):
-    empty_value_display = '-пусто-'
--- backend/core/paginations.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-from rest_framework.pagination import PageNumberPagination
-
-
-class Pagination(PageNumberPagination):
-    page_size = settings.PAGE_SIZE
