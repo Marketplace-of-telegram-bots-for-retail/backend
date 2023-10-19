46f1767c8efd6d20e03b5d4b3c7699d9632983e4
Артем Пилипенко <artem38skull@yandex.ru>
2023-10-19 21:14:16 +0800

    Fix is_in_shopping_cart

--- backend/core/utils.py
+++
@@ -1,8 +0,0 @@
-def checking_existence(user, object, model):
-    if user.is_anonymous:
-        return False
-    else:
-        return model.objects.filter(
-            user=user,
-            product=object.id,
-        ).exists()
