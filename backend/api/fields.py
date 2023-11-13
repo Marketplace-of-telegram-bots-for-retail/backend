edb42a375b8d28df82d2b99223c34096c5253158
Артем Пилипенко <artem38skull@yandex.ru>
2023-11-13 19:24:26 +0800

    Update image name

--- backend/api/fields.py
+++ backend/api/fields.py
@@ -9,7 +9,7 @@ class Base64ImageField(serializers.ImageField):
         if isinstance(data, str) and data.startswith('data:image'):
             format, imgstr = data.split(';base64,')
             ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='bot.' + ext)
+            data = ContentFile(base64.b64decode(imgstr), name='image.' + ext)
         return super().to_internal_value(data)
 
 
