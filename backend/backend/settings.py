d579cef016142f3cb6fd0e5a739a20b09ff746d4
Виктор Тузовский <vtuzovskiy@fura.ru>
2023-12-07 18:19:49 +0700

    Added new server ip in csrf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -182,7 +182,7 @@ FIRST_ORDER_NUMBER = 100000
 
 CSRF_TRUSTED_ORIGINS = [
     'https://botmarketplace.ru',
-    'https://80.87.109.115',
+    'https://45.155.207.42',
     'http://localhost:3000',
     'https://marketplace-of-telegram-bots-for-retail.github.io',
 ]
